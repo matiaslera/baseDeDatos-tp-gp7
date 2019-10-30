06f1cc447a1a71233d2a5160deb764add524c0e6
nicolas <kirinico92@gmail.com>
2019-10-30 20:06:42 -0300

    refactor de la pantalla AMB - Encuesta

--- src/app/componentes/poll/poll.component.ts
+++ src/app/componentes/poll/poll.component.ts
@@ -4,20 +4,20 @@ import { trigger, state, transition, style, animate } from '@angular/animations'
 export interface records {
   name: string;
   description: string;
-  action: string;
+  
 }
 
 const ELEMENT_DATA: records[] = [
-  {name: 'Hydrogen', description: '1.0079', action: 'H'},
-  {name: 'Helium', description: '4.0026', action: 'He'},
-  {name: 'Lithium', description:' 6.941', action: 'Li'},
-  {name: 'Beryllium', description: '9.0122', action: 'Be'},
-  {name: 'Boron', description: '10.811', action: 'B'},
-  {name: 'Carbon', description: '12.0107', action: 'C'},
-  {name: 'Nitrogen', description: '14.0067', action: 'N'},
-  {name: 'Oxygen', description: '15.9994', action: 'O'},
-  {name: 'Fluorine', description: '18.9984', action: 'F'},
-  {name: 'Neon', description: '20.1797', action: 'Ne'},
+  {name: 'Hydrogen', description: '1.0079'},
+  {name: 'Helium', description: '4.0026'},
+  {name: 'Lithium', description:' 6.941'},
+  {name: 'Beryllium', description: '9.0122'},
+  {name: 'Boron', description: '10.811'},
+  {name: 'Carbon', description: '12.0107'},
+  {name: 'Nitrogen', description: '14.0067'},
+  {name: 'Oxygen', description: '15.9994'},
+  {name: 'Fluorine', description: '18.9984'},
+  {name: 'Neon', description: '20.1797'},
 ];
 
 @Component({
--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,18 +1,20 @@
 import { NgModule } from '@Angular/core'
 
-import { MatButtonModule, MatInputModule, MatCardModule, MatTableModule }from '@Angular/material'
+import { MatButtonModule, MatInputModule, MatCardModule, MatTableModule, MatIconModule }from '@Angular/material'
 @NgModule({
     imports: [
         MatButtonModule,
         MatInputModule,
         MatCardModule,
-        MatTableModule
+        MatTableModule,
+        MatIconModule
     ],
     exports: [
         MatButtonModule,
         MatInputModule,
         MatCardModule,
-        MatTableModule
+        MatTableModule,
+        MatIconModule
     ]
 })
 export class MaterialModule { }
