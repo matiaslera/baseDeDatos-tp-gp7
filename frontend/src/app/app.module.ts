cb14fc9fcc437a592dad6851f73e3a56c5ebb79c
kirinico <kirinico@gmail.com>
2019-11-06 23:16:57 -0300

    implementacon filter buscar pantalla descargas

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { NewButtonComponent } from './componentes/newButton/newButton.component'
 import { AdminOfPollComponent } from './componentes/adminOfPoll/adminOfPoll.component';
 import { ToolbarComponent } from './componentes/toolbar/toolbar.component';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
    declarations: [
@@ -32,7 +33,8 @@ import { HttpClientModule } from '@angular/common/http';
       AppRoutingModule,
       BrowserAnimationsModule,
       MaterialModule,
-      HttpClientModule
+      HttpClientModule,
+      FormsModule,
    ],
    providers: [],
    bootstrap: [
--- frontend/src/app/componentes/download/download.component.ts
+++ frontend/src/app/componentes/download/download.component.ts
@@ -3,18 +3,7 @@ import { Router } from '@angular/router';
 import { DownloadService } from 'src/app/service/download/Download.service';
 import { Download } from 'src/app/domain/Download';
 
-export interface ContensElement {
-  title: string;
-  extension: string;
-  fecha: string
-}
 
-const ELEMENT_CONTENS: ContensElement[] = [
-  { title: 'Game of thones', extension: '1.0079', fecha: '2019-01-01' },
-  { title: 'gladiador', extension: '4.0026', fecha: '2019-01-01' },
-  { title: 'los picapiedras', extension: '18.9984', fecha: '2019-01-01' },
-  { title: 'SAO', extension: '20.1797', fecha: '2019-01-01' },
-];
 
 
 @Component({
@@ -23,11 +12,11 @@ const ELEMENT_CONTENS: ContensElement[] = [
   styleUrls: ['./download.component.css']
 })
 export class DownloadComponent implements OnInit {
-
+  download: Download = new Download
   displayedColumns: string[] = ['title', 'extension', 'fecha', "action"];
-  dataSource = ELEMENT_CONTENS;
   constructor(private router: Router, private downloadService: DownloadService) { }
   downloads: Download[]
+  backend: Download[]
   navegatePoll() {
     this.router.navigateByUrl('home/descargas/editar');
   }
@@ -35,9 +24,19 @@ export class DownloadComponent implements OnInit {
   async ngOnInit() {
     try {
       this.downloads = await this.downloadService.getdownloads()
+      this.backend =  this.downloads
     } catch (error) {
       console.error(error)
     }
+  }
+
+  filterDownload(nameDownload){
+    this.downloads = this.backend
+    console.log(nameDownload.value)
+    this.downloads = this.downloads.filter(expediente =>
+      expediente.titulo.indexOf(nameDownload.value) >= 0
+);
+  console.log(this.downloads)
   }
 
 }
